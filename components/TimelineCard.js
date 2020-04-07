87ba447855ae9cddebb2e65ec0b57b55a382a09a
jgough99 <jamgough@outlook.com>
2020-04-07 17:31:05 +0100

    Timeline updates

--- components/TimelineCard.js
+++ components/TimelineCard.js
@@ -1,23 +1,87 @@
 import { Card, Button } from "react-native-elements";
-import { Text } from "react-native";
+import { Text, View, ActivityIndicator, Image } from "react-native";
 import React, { Component } from "react";
 import * as Constants from "../Constants";
+import * as firebase from "firebase";
+import firestore from "@firebase/firestore";
+var ref = null;
+var url = null;
 
-export default function TimelineCard(props) {
-  return (
-    <Card
-      image={props.imagePath}
-      containerStyle={{ elevation: 6, borderRadius: 15, overflow: "hidden" }}
-    >
-      <Text style={{ fontSize: 24 }}>{props.title}</Text>
-      <Text style={{ fontSize: 14, color: "#737272" }}>{props.type}</Text>
+export default class TimelineCard extends React.Component {
+  state = {
+    title: "",
+    type: "",
+    image: "",
+    loading: true,
+  };
 
-      <Button
-        containerStyle={{ alignSelf: "baseline" }}
-        type="clear"
-        title="DETAILS"
-        titleStyle={{ color: Constants.colors.primary, fontSize: 12 }}
-      />
-    </Card>
-  );
+  async componentDidMount() {
+    await this.getEvent();
+  }
+
+  async getEvent() {
+    const db = firebase.firestore();
+    db.collection("event")
+      .doc(this.props.eventId)
+      .get()
+      .then(async (doc) => {
+        this.setState({ title: doc.data().title });
+        this.setState({ type: doc.data().type });
+        this.setState({ image: doc.data().userId + doc.data().title });
+        ref = firebase.storage().ref("images/" + this.state.image);
+        url = await ref.getDownloadURL();
+        this.setState({ loading: false });
+      });
+  }
+
+  cardContents() {
+    if (!this.state.loading) {
+      return (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text style={{ fontSize: 24, marginLeft: 10 }}>
+            {this.state.title}
+          </Text>
+          <Text style={{ fontSize: 14, color: "#737272", marginLeft: 10 }}>
+            {this.state.type}
+          </Text>
+
+          <Button
+            containerStyle={{ alignSelf: "baseline" }}
+            type="clear"
+            title="DETAILS"
+            titleStyle={{
+              color: Constants.colors.primary,
+              fontSize: 12,
+              marginLeft: 6,
+            }}
+          />
+        </View>
+      );
+    } else {
+      return <ActivityIndicator size="large" color="grey" />;
+    }
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          marginTop: 20,
+          borderRadius: 15,
+          overflow: "hidden",
+          minWidth: "90%",
+          maxHeight: Constants.windowHeight * 0.35,
+          borderWidth: 1,
+          borderColor: "#CDCBCB",
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Image style={{ minHeight: "50%" }} source={{ uri: url }} />
+        </View>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          {this.cardContents()}
+        </View>
+      </View>
+    );
+  }
 }
--- screens/Timeline.js
+++ screens/Timeline.js
@@ -1,45 +1,112 @@
 import * as React from "react";
-import { Text, View, Image, SafeAreaView, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import { Header, Card, Button } from "react-native-elements";
 import CustomHeader from "../components/Header";
 import TimelineCard from "../components/TimelineCard";
 import * as Constants from "../Constants";
-export default function Timeline() {
-  return (
-    <View style={{ flex: 1 }}>
-      <CustomHeader title="Timeline" />
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Image
-          source={require("../assets/flags.png")}
-          fadeDuration={0}
-          style={{
-            width: Constants.windowWidth * 0.9,
-            height: Constants.windowWidth * 0.6,
-          }}
-        />
-        <Text
-          style={{
-            width: "70%",
-            textAlign: "center",
-            marginTop: 10,
-            fontFamily: "Rubik-Regular",
-            fontSize: 22,
-          }}
-        >
-          Your timeline is empty
-        </Text>
-        <Text
-          style={{
-            width: "60%",
-            textAlign: "center",
-            marginTop: 10,
-            fontFamily: "Rubik-Regular",
-            fontSize: 15,
-          }}
-        >
-          When you attend an event, it will show up here.
-        </Text>
-      </View>
-    </View>
-  );
+import * as firebase from "firebase";
+import firestore from "@firebase/firestore";
+
+export default class Timeline extends React.Component {
+  state = {
+    events: [],
+    noEvents: null,
+  };
+
+  componentDidMount() {
+    this.getEventList();
+  }
+
+  onScreenFocus = () => {
+    this.componentDidMount();
+  };
+
+  getEventList() {
+    const db = firebase.firestore();
+
+    db.collection("attendance")
+      .where("userId", "==", firebase.auth().currentUser.uid.toString())
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          this.setState({ noEvents: true });
+          return;
+        }
+        snapshot.forEach((doc) => {
+          this.state.events.push(doc.data().eventId);
+        });
+        this.setState({ noEvents: false });
+
+        this.getEventDetails();
+      })
+
+      .catch((err) => {
+        console.log("Error getting documents", err);
+      });
+  }
+
+  render() {
+    if (this.state.noEvents == false) {
+      return (
+        <View style={{ flex: 1 }}>
+          <CustomHeader title="Timeline" />
+          <View style={{ flex: 1, alignItems: "center", minWidth: "100%" }}>
+            {this.state.events.map((event, index) => (
+              <TimelineCard eventId={event} />
+            ))}
+          </View>
+        </View>
+      );
+    }
+    if (this.state.noEvents == true) {
+      return (
+        <View style={{ flex: 1 }}>
+          <CustomHeader title="Timeline" />
+          <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          >
+            <Image
+              source={require("../assets/flags.png")}
+              fadeDuration={0}
+              style={{
+                width: Constants.windowWidth * 0.9,
+                height: Constants.windowWidth * 0.6,
+              }}
+            />
+            <Text
+              style={{
+                width: "70%",
+                textAlign: "center",
+                marginTop: 10,
+                fontFamily: "Rubik-Regular",
+                fontSize: 22,
+              }}
+            >
+              Your timeline is empty
+            </Text>
+            <Text
+              style={{
+                width: "60%",
+                textAlign: "center",
+                marginTop: 10,
+                fontFamily: "Rubik-Regular",
+                fontSize: 15,
+              }}
+            >
+              When you attend an event, it will show up here.
+            </Text>
+          </View>
+        </View>
+      );
+    } else {
+      return <ActivityIndicator size="large" color="grey" />;
+    }
+  }
 }
